d65a59f3d4830a297f74ae2e13d19ab1081f1d5a
MHWillard <mhwillard@gmail.com>
2021-05-15 14:26:36 -0400

    adding env file and Heroku read file

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+
+require('dotenv').config()
+const port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000;
+}
 
 app.get('/', (req, res) => {
     res.send('Backend online.');
