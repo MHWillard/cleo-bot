510fa415cd4e08ae0727568ae59d420c7027cda8
MHWillard <mhwillard@gmail.com>
2021-05-17 13:30:30 -0400

    dayjs add

--- timer.js
+++ timer.js
@@ -3,5 +3,7 @@
 //subtract from time in show: 01/01/2525
 //create and return print out: number of years and days
 
+const dayjs = require('dayjs');
+
 let now = dayjs();
 let future = dayjs(new Date(2525,1,1));
