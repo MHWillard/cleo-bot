e0776b849012795c56beb9da88d192fd65b410f9
MHWillard <mhwillard@gmail.com>
2021-05-19 06:53:29 -0400

    day.js timer

--- timer.js
+++ timer.js
@@ -3,11 +3,16 @@
 //subtract from time in show: 01/01/2525
 //create and return print out: number of years and days
 
+//require dayjs and plugins
 const dayjs = require('dayjs');
 const relativeTime = require('dayjs/plugin/relativeTime');
 const duration = require('dayjs/plugin/duration');
+const dayOfYear = require('dayjs/plugin/dayOfYear')
+
+//Extend plugins
 dayjs.extend(relativeTime);
 dayjs.extend(duration);
+dayjs.extend(dayOfYear);
 
 let now = dayjs();
 let future = dayjs('2525-01-01');
@@ -18,11 +23,21 @@ let future = dayjs('2525-01-01');
 //let countdownString = countdown.format('MM/DD/YYYY');
 //et countdownString = countdown;
 
+//get current day of year
+let currentDay = dayjs(now).dayOfYear()
+
+let currentYear = dayjs(now).year().toString();
+
+let lastDayOfCurrentYear = dayjs(currentYear + '-12-31').dayOfYear()
+
 const date = future.diff(now, 'day');
 const years_left = Math.round((date / 365));
-const days_left = (years_left % 365)
+//count number of days of current year and subtract current passed days to get days left
+const days_left = (lastDayOfCurrentYear - currentDay)
 
 console.log('Years left:' + years_left);
 console.log('days left:' + days_left);
+console.log(currentDay);
+console.log(currentYear);
 
 module.exports = {years_left, days_left}
