f843a8861ec4bcdbec7fc14676ad332ae0190e10
MHWillard <mhwillard@gmail.com>
2021-05-20 13:01:01 -0400

    starting API connection

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cron = require('node-cron');
 const {years_left, days_left} = require('./timer.js');
+const {T} = require('./twitter')
 
 require('dotenv').config()
 const port = process.env.PORT;
@@ -11,8 +12,9 @@ if (port == null || port == "") {
 
 app.get('/', (req, res) => {
     res.send('Backend online.');
-    console.log('Years left: ' + years_left);
-    console.log('Days left: ' + days_left);
+    T.get('search/tweets', {q: 'batman', count: 10}, function(err, data, response) {
+        console.log(data);
+    });
     //run date countdown
     //if countdown is good: build and fire tweet to API as POST request
     //this cron job, when finished, will run every 24 hours and create the scheduled tweet
--- twitter.js
+++ twitter.js
@@ -15,22 +15,28 @@
 
 
 //create new Twit object with API connection
-var Twit = require('twit');
+let Twit = require('twit');
 
-//var T = new Twit({});
-
-//prepare scheduled tweet object
-
-T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
-    console.log(data)
-  })
-
-  //T.post/get(path, param, callback)
-T.get('search/tweets', {q: 'batman', count: 100}, function(err, data, response) {
-    console.log(data);
+let T = new Twit({
+    consumer_key: process.env.API_KEY,
+    consumer_secret: process.env.API_SECRET_KEY,
+    app_only_auth: true
 });
+
+//connect and authenticate with Twitter API app
+//run get search from Twiter
+//dump to console
+
+//can I get this into an object?
+//T.get('search/tweets', {q: 'batman', count: 10}, function(err, data, response) {
+    //console.log(data);
+//});
 
 //https://ads-api.twitter.com/9/accounts/:account_id/scheduled_tweets
 //account_id: ID for account used
 //scheduled_at
 //test: search for tweets to log to console and test connection through main account
+
+//prepare scheduled tweet object
+
+module.exports = {T}
