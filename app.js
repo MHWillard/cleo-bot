ddd37b0b08ea479caab8007a530c2c64cbc030ec
MHWillard <mhwillard@gmail.com>
2021-05-22 13:36:40 -0400

    heroku scheduler

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const port = process.env.PORT;
 if (port == null || port == "") {
     port = 3000;
 }
-res.send('Backend online.');
+//res.send('Backend online.');
 console.log('There are ' + years_left + ' years and ' + days_left + ' days left until the events of Cleopatra 2525! https://www.youtube.com/watch?v=e2ZRSwul7cE');
 T.get('search/tweets', {q: 'batman', count: 1}, function(err, data, response) {
     console.log(data);
