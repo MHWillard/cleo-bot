1ef12689940f860dfaa3f458847adfc64e01e957
MHWillard <mhwillard@gmail.com>
2021-05-22 13:28:37 -0400

    testing heroku scheduler

--- app.js
+++ app.js
@@ -9,18 +9,20 @@ const port = process.env.PORT;
 if (port == null || port == "") {
     port = 3000;
 }
-
-app.get('/', (req, res) => {
-    res.send('Backend online.');
-    console.log('There are ' + years_left + ' years and ' + days_left + ' days left until the events of Cleopatra 2525! https://www.youtube.com/watch?v=e2ZRSwul7cE');
-    T.get('search/tweets', {q: 'batman', count: 1}, function(err, data, response) {
-        console.log(data);
-    });
+res.send('Backend online.');
+console.log('There are ' + years_left + ' years and ' + days_left + ' days left until the events of Cleopatra 2525! https://www.youtube.com/watch?v=e2ZRSwul7cE');
+T.get('search/tweets', {q: 'batman', count: 1}, function(err, data, response) {
+    console.log(data);
 });
 
-app.listen(port, (req, res)=> {
-    console.log('Backend running on port ' + port + '.');
-});
+//app.listen(port, (req, res)=> {
+    //console.log('Backend running on port ' + port + '.');
+//});
+
+
+//app.get('/', (req, res) => {
+//
+//});
 
 //cron.schedule('* * * * *', () => {
     //console.log('There are ' + years_left + ' years and ' + days_left + ' days left until the events of Cleopatra 2525! https://www.youtube.com/watch?v=e2ZRSwul7cE');
