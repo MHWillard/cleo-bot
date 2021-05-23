e6d79eac84c417f13af8184972e16eaa01731dad
MHWillard <mhwillard@gmail.com>
2021-05-23 19:30:32 -0400

    readjusting script to point to bot account

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
-const app = express();
+//const app = express();
 require('dotenv').config()
-const cron = require('node-cron');
 const {years_left, days_left} = require('./timer.js');
 const {T} = require('./twitter')
 
@@ -9,24 +8,12 @@ const port = process.env.PORT;
 if (port == null || port == "") {
     port = 3000;
 }
-//res.send('Backend online.');
-console.log('There are ' + years_left + ' years and ' + days_left + ' days left until the events of Cleopatra 2525! https://www.youtube.com/watch?v=e2ZRSwul7cE');
+console.log('There are ' + years_left + ' years and ' + days_left + ' days left until the events of Cleopatra 2525!\nhttps://www.youtube.com/watch?v=e2ZRSwul7cE');
+
 T.get('search/tweets', {q: 'batman', count: 1}, function(err, data, response) {
     console.log(data);
 });
 
-//app.listen(port, (req, res)=> {
-    //console.log('Backend running on port ' + port + '.');
-//});
-
-
-//app.get('/', (req, res) => {
-//
-//});
-
-//cron.schedule('* * * * *', () => {
-    //console.log('There are ' + years_left + ' years and ' + days_left + ' days left until the events of Cleopatra 2525! https://www.youtube.com/watch?v=e2ZRSwul7cE');
-    //T.get('search/tweets', {q: 'superman', count: 1}, function(err, data, response) {
-      //  console.log(data);
-    //});
-//})
+T.post('statuses/update', { status: 'Testing Twitter Dev API...' }, function(err, data, response) {
+    console.log(data)
+  })
--- twitter.js
+++ twitter.js
@@ -1,42 +1,11 @@
-//fetch current EST from API
-//convert
-//compare with set timeframe (probably like 7 PM EST)
-//if true: build and post scheduled tweet (or use twitter API for this)
-
-//TWITTER API CONNECTION:
-//first, establish twitter API connection object
-
-//TWITTER API ENDPOINTS:
-//POST accounts/:account_id/scheduled_tweets (create a new Scheduled Tweet)
-//build the tweet post object and send it out at scheduled time
-//use twitter connection object
-
-//last: if needed, close connection
-
-
 //create new Twit object with API connection
 let Twit = require('twit');
 
 let T = new Twit({
     consumer_key: process.env.API_KEY,
     consumer_secret: process.env.API_SECRET_KEY,
-    app_only_auth: true
+    access_token: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.SECRET_ACCESS
 });
-
-//connect and authenticate with Twitter API app
-//run get search from Twiter
-//dump to console
-
-//can I get this into an object?
-//T.get('search/tweets', {q: 'batman', count: 1}, function(err, data, response) {
-    //console.log(data);
-//});
-
-//https://ads-api.twitter.com/9/accounts/:account_id/scheduled_tweets
-//account_id: ID for account used
-//scheduled_at
-//test: search for tweets to log to console and test connection through main account
-
-//prepare scheduled tweet object
 
 module.exports = {T}
